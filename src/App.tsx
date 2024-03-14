0118d6515db1c727b4cdefa344a6f98f82bc6b11
Janos Kocs <janos.kocs@outlook.com>
2024-03-14 12:42:15 +0000

    feature: implement keyboard press

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import words from "./data/wordList.json";
 import HangmanDrawing from "./components/HangmanDrawing";
 import HangmanWord from "./components/HangmanWord";
@@ -11,6 +11,29 @@ const App = () => {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
   const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+
+  const addGuessedLetter = (letter: string) => {
+    if (guessedLetters.includes(letter)) return
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    
+  }
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      const key = event.key;
+
+      if (!key.match(/^[a-z]$/)) return;
+
+      event.preventDefault();
+      addGuessedLetter(key);
+    }
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+  }, [])
 
   return (
     <div style={{ maxWidth: "800px", display: "flex", flexDirection: "column", alignItems: "center", margin: "0 auto", gap: "2rem" }}>
